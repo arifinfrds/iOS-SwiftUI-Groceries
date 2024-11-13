118c3e29ed17f568b817d2b86da1848ae770f977
Arifin Firdaus <arifinfrds.engineer@gmail.com>
2024-11-13 09:53:04 +0700

    Create base Test target

---
+++ GroceriesTests/GroceriesViewModelTests.swift
@@ -0,0 +1,16 @@
+//
+//  GroceriesViewModelTests.swift
+//  GroceriesTests
+//
+//  Created by arifin on 13/11/24.
+//
+
+import XCTest
+
+final class GroceriesViewModelTests: XCTestCase {
+    
+    func testInit() {
+        
+    }
+
+}
