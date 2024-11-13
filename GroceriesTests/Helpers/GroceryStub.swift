a8adbc77127ba14bef6d4fff0cd22a97e79703e5
Arifin Firdaus <arifinfrds.engineer@gmail.com>
2024-11-13 11:23:22 +0700

    Move `GroceryStub` to dedicated file

---
+++ GroceriesTests/Helpers/GroceryStub.swift
@@ -0,0 +1,75 @@
+//
+//  GroceryStub.swift
+//  GroceriesTests
+//
+//  Created by arifin on 13/11/24.
+//
+
+@testable import Groceries
+import Foundation
+
+// MARK: - Helpers
+
+final class GroceryStub: GroceryLoader, GroceryDeleter, GroceryAdder {
+    
+    enum Invocation {
+        case loaderGroceries
+        case deleteGroceries
+        case delete
+        case add
+    }
+    
+    private(set) var invocations = [Invocation]()
+    
+    private let loadGroceriesResult: Result<[GroceryItem], any Error>
+    private let deleteGroceriesResult: Result<Void, any Error>
+    private let deleteGroceryResult: Result<Void, any Error>
+    private let addGroceryResult: Result<Void, any Error>
+    
+    init(
+        loadGroceriesResult: Result<[GroceryItem], any Error> = .failure(NSError()),
+        deleteGroceriesResult: Result<Void, any Error> = .failure(NSError()),
+        deleteGroceryResult: Result<Void, any Error> = .failure(NSError()),
+        addGroceryResult: Result<Void, any Error> = .failure(NSError())
+    ) {
+        self.loadGroceriesResult = loadGroceriesResult
+        self.deleteGroceriesResult = deleteGroceriesResult
+        self.deleteGroceryResult = deleteGroceryResult
+        self.addGroceryResult = addGroceryResult
+    }
+    
+    func loadGroceries() async throws -> [GroceryItem] {
+        invocations.append(.loaderGroceries)
+        return try loadGroceriesResult.get()
+    }
+    
+    func deleteGroceries() async throws {
+        invocations.append(.deleteGroceries)
+        switch deleteGroceriesResult {
+        case .success:
+            break
+        case .failure(let error):
+            throw error
+        }
+    }
+    
+    func delete(grocery: GroceryItem) async throws {
+        invocations.append(.delete)
+        switch deleteGroceryResult {
+        case .success:
+            return
+        case .failure(let error):
+            throw error
+        }
+    }
+    
+    func add(grocery: GroceryItem) async throws {
+        invocations.append(.add)
+        switch addGroceryResult {
+        case .success:
+            return
+        case .failure(let error):
+            throw error
+        }
+    }
+}
