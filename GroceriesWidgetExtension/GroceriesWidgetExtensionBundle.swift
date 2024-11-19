b5b0ad450eba674d2e7b230760564af93a929033
Arifin Firdaus <arifinfrds.engineer@gmail.com>
2024-11-19 09:08:54 +0700

    Create base `GroceriesWidgetExtension` target

---
+++ GroceriesWidgetExtension/GroceriesWidgetExtensionBundle.swift
@@ -0,0 +1,16 @@
+//
+//  GroceriesWidgetExtensionBundle.swift
+//  GroceriesWidgetExtension
+//
+//  Created by arifin on 19/11/24.
+//
+
+import WidgetKit
+import SwiftUI
+
+@main
+struct GroceriesWidgetExtensionBundle: WidgetBundle {
+    var body: some Widget {
+        GroceriesWidgetExtension()
+    }
+}
