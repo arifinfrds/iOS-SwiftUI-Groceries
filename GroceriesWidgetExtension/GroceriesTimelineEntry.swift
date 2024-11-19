d943731d91a0afbcc349d56e0f0e1f252280aeb0
Arifin Firdaus <arifinfrds.engineer@gmail.com>
2024-11-19 09:11:12 +0700

    Creates `GroceriesTimelineEntry`

---
+++ GroceriesWidgetExtension/GroceriesTimelineEntry.swift
@@ -0,0 +1,13 @@
+//
+//  GroceriesTimelineEntry.swift
+//  GroceriesWidgetExtensionExtension
+//
+//  Created by arifin on 19/11/24.
+//
+
+import WidgetKit
+
+struct GroceriesTimelineEntry: TimelineEntry {
+    let date: Date
+    let grocery: String
+}
